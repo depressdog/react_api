b6d5fe7224ba43f3397767114627dbe269fd4edf
doges <dante31.3d@gmail.com>
2019-04-01 04:00:40 +0300

    add sub cat api

---
+++ app/controllers/api/v1/subsubcategories_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::SubsubcategoriesController < ApplicationController
+  def index
+    @subsubcategories = Subsubcategory.order("subcategory_id")
+
+    render json: @subsubcategories
+  end
+
+  def create
+    @subsubcategories = Subsubcategory.new(subsubcat_params)
+
+    if @subsubcategories.save
+      render json: @subsubcategories
+    else
+      render json: @subsubcategories.errors
+    end
+  end
+
+  def update
+    @subsubcategories = Subsubcategory.find(params[:id])
+    @subsubcategories.update_attributes(subsubcat_params)
+    render json: @subsubcategories
+  end
+  def destroy
+    @subsubcategories = Subsubcategory.find(params[:id])
+    if @subsubcategories.destroy
+      head :no_content, status: :ok
+    else
+      render json: @subsubcategories.errors, status: :unprocessable_entity
+    end
+  end
+  private
+    def subsubcat_params
+      params.require(:subsubcategory).permit(:name, :subcategory_id )
+    end
+end
---
+++ app/javascript/components/IndexSubSubCategories.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import axios from 'axios'
+import update from 'immutability-helper'
+
+class IndexSubSubCategories extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			subsubcategories: [],
+			editSubId: ''
+		}
+	}
+	render() {
+		return(
+			<React.Fragment>
+				lorem
+			</React.Fragment>
+		)
+	}
+}
+export default IndexSubSubCategories

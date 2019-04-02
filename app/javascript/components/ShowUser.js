738239ee47098b16981934531ac6398f27b01cab
doges <dante31.3d@gmail.com>
2019-04-02 22:52:11 +0300

    edit user update and user page

---
+++ app/javascript/components/ShowUser.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import axios from 'axios'
+
+class ShowUser extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className="ui grid">
+					<div className="five wide column">
+						<div className="ui medium circular image">
+							<img src="https://semantic-ui.com/images/avatar2/large/molly.png" alt=""/>
+						</div>
+						<a href="http://localhost:3000/users/edit" className="fluid ui button blue">редактировать</a>
+					</div>
+					<div className="eleven wide column">
+						<div className="ui list">
+							<div className="item">
+								Ник:
+							</div>
+							<div className="item">
+								Номер телефона:
+							</div>
+							<div className="item">
+								Имейл:
+							</div>
+							<div className="item">
+								Образование:
+							</div>
+							<div className="item">
+								мои навыки:
+								<a href="#" className="ui label">Tag</a>
+								<a href="#" className="ui label">Tag</a>
+								<a href="#" className="ui label">Tag</a>
+								<a href="#" className="ui label">Tag</a>
+							</div>
+							<div className="item">
+								Мой возраст:
+							</div>
+							<div className="item">
+								Локация:
+							</div>
+							<div className="item">
+								О себе:
+							</div>
+							<div className="item">
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		)
+	}
+}
+export default ShowUser

83362ecbb1c673931ca75cf30169c68e978e7148
doges <dante31.3d@gmail.com>
2019-04-05 07:53:45 +0300

    fixing json cities 2

--- app/javascript/components/IndexCities.js
+++ app/javascript/components/IndexCities.js
@@ -31,7 +31,7 @@ class IndexCities extends React.Component {
     }
 
     addNewCat(name, country_id) {
-        axios.post( '//masterzz.club/api/v1/cities', { city: {name: name, country_id: country_id} })
+        axios.post( '/api/v1/cities', { city: {name: name, country_id: country_id} })
             .then(response => {
                 console.log(response);
                 const cities = update(this.state.cities, {

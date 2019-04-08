ca73ac170cc99637b7c232fa378992af65e6c583
depressdog <dante31.3d@gmail.com>
2019-04-08 15:36:05 +0300

    work json

--- app/javascript/components/axiosClient.js
+++ app/javascript/components/axiosClient.js
@@ -1,7 +1,8 @@
 let axios = require('axios');
 
 let axiosClient = axios.create({
-    baseURL: '//localhost:3000/api/v1/'
+    // baseURL: '//localhost:3000/api/v1/'
+    baseURL: '//masterzz.club/api/v1/'
 });
 
 export default axiosClient;

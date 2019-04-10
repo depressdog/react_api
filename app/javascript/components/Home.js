6cc0691bb2bd9b5d6ee4a934ed582934085c40cf
depressdog <dante31.3d@gmail.com>
2019-04-10 15:26:16 +0300

    home images 2

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -6,11 +6,11 @@ class Home extends React.Component {
 		super(props);
 		this.state = {
 			homes: [
-				{id: 1, title: 'Обучение', imgUrl: 'matthew-b1904dc651ba20c86e1892d37c0193359b7351704e4716cf5fe6fb860ef22935.png'},
-				{id: 2, title: 'тренинги', imgUrl: '/assets/matthew.png'},
-				{id: 3, title: 'работа', imgUrl: '/assets/matthew.png'},
-				{id: 4, title: 'Услуги', imgUrl: '/assets/matthew.png'},
-				{id: 5, title: 'курсы', imgUrl: '/assets/matthew.png'}
+				{id: 1, title: 'Обучение', imgUrl: '/assets/matthew-b1904dc651ba20c86e1892d37c0193359b7351704e4716cf5fe6fb860ef22935.png'},
+				{id: 2, title: 'тренинги', imgUrl: '/assets/matthew-b1904dc651ba20c86e1892d37c0193359b7351704e4716cf5fe6fb860ef22935.png'},
+				{id: 3, title: 'работа', imgUrl: '/assets/matthew-b1904dc651ba20c86e1892d37c0193359b7351704e4716cf5fe6fb860ef22935.png'},
+				{id: 4, title: 'Услуги', imgUrl: '/assets/matthew-b1904dc651ba20c86e1892d37c0193359b7351704e4716cf5fe6fb860ef22935.png'},
+				{id: 5, title: 'курсы', imgUrl: '/assets/matthew-b1904dc651ba20c86e1892d37c0193359b7351704e4716cf5fe6fb860ef22935.png'}
 			]
 		}
 	}
